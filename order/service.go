3a58b0d073c97569408d596eddccd2343834a905
asraful-07 <asraful0508@gmail.com>
2025-10-08 10:03:10 +0600

    add service

--- order/service.go
+++ order/service.go
@@ -15,3 +15,19 @@ func NewService(orderRepo OrderRepo) Service {
 func (svc *service) Create(order domain.Order) (*domain.Order, error) {
 	return svc.orderRepo.Create(order)
 }
+
+func (svc *service) Get(id int) (*domain.Order, error) {
+	return svc.orderRepo.Get(id)
+}
+
+func (svc *service) List() ([]*domain.Order, error) {
+	return  svc.orderRepo.List()
+}
+
+func (svc *service) Update(o domain.Order) (*domain.Order, error) {
+	return svc.orderRepo.Update(o)
+}
+
+func (svc *service) Delete(id int) error {
+	return svc.orderRepo.Delete(id)
+}
