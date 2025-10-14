6741759f912e9cf3c1daad2845e7dbb3a8d2dc93
asraful-07 <asraful0508@gmail.com>
2025-10-14 09:30:23 +0600

    add review

--- repo/review.go
+++ repo/review.go
@@ -3,6 +3,7 @@ package repo
 import (
 	"books/domain"
 	"books/review"
+	"database/sql"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -20,22 +21,89 @@ func NewReviewRepo(db *sqlx.DB)  ReviewRepo {
 	return &reviewRepo{db: db}
 }
 
-func (r *reviewRepo ) Create(b domain.Review) (*domain.Review, error) {
+func (r *reviewRepo ) Create(review domain.Review) (*domain.Review, error) {
 	query := `
-		INSERT INTO reviews (email, description, review_date)
-		VALUES ($1, $2, $3)
-		RETURNING id
+	INSERT INTO reviews (email, description, review_date)
+	VALUES ($1, $2, $3)
+	RETURNING id
 	`
+
 	err := r.db.QueryRow(
 		query,
-		b.Email,
-		b.Description,
-		b.ReviewDate,
-	).Scan(&b.ID)
+		review.Email,
+		review.Description,
+		review.ReviewDate,
+	).Scan(review.ID)
 
 	if err != nil {
-		return nil, err
+		return  nil, err
 	}
+	return  &review, nil;
+}
 
-	return &b, nil
+
+func (r *reviewRepo) Get(id int) (*domain.Review, error) {
+	var review domain.Review
+	query := `
+	SELECT id, email, description, review_date
+	FROM reviews
+	WHERE id = $1
+	`
+
+	err := r.db.Get(&review, query, id)
+	if err != nil {
+		if err == sql.ErrNoRows{
+			return  nil, nil
+		}
+		return  nil, err
+	}
+	return &review, nil
+}
+
+func (r *reviewRepo) List() ([]*domain.Review, error) {
+	var review []*domain.Review
+	query := `
+	SELECT id, email, description, review_date
+	FROM reviews
+	ORDER BY id
+	`
+
+	err := r.db.Select(&review, query)
+	if err != nil {
+		return  nil, err
+	}
+	return  review, nil
+}
+
+
+func (r *reviewRepo) Update(review domain.Review) (*domain.Review, error) {
+	query := `
+	UPDATE reviews
+	SET email = :email,
+	    description = :description,
+		review_date = :review_date,
+    WHERE id = :id
+	RETURNING id
+	`
+
+	rows, err := r.db.NamedQuery(query, review)
+	if err != nil {
+		return  nil, err
+	}
+	defer rows.Close()
+	if rows.Next() {
+	  if err:= rows.Scan(review.ID); err != nil {
+		return  nil, err
+	  }
+	  return nil, err
+	}
+	return &review, nil
+}
+
+func (r *reviewRepo) Delete(reviewID int) error {
+	query := `DELETE FORM reviews WHERE id = $1`
+
+	_, err := r.db.Exec(query, reviewID)
+
+	return err
 }
